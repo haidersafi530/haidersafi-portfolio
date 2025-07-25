1acd857d293adde1db8ddc53d97851af7b43fdce
haidersafi530 <haidersafi2000@gmail.com>
2025-07-25 22:13:37 +0500

    Portfolio Breaking Down Page Logic

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
-var tl = gsap.timeline();
-tl.to('#fs', {
-    height: 0,
-    duration: 2,
-    ease: Expo.easeInOut    
-})
-.to('#elem', {
-    height: "100%",
-    duration: 2,
-    delay: -2,
-    ease: Expo.easeInOut
+document.querySelectorAll('.reveal').forEach(function(elem){
+    let parent = document.createElement('span');
+    let child = document.createElement('span');
+
+    parent.classList.add('parent');
+    child.classList.add('child');
+
+    child.textContent = elem.textContent;
+    parent.appendChild(child);
+    console.log(parent);
+
+    elem.innerHTML = "";
+    elem.appendChild(parent);
+
 })
