7366b40eb40e4067fe3a0f16fe0455070ced7d61
haidersafi530 <haidersafi2000@gmail.com>
2025-07-26 01:07:21 +0500

    Portfolio Loader Page Animation With Gsap Lbry

--- script.js
+++ script.js
@@ -1,15 +1,55 @@
-document.querySelectorAll('.reveal').forEach(function(elem){
-    let parent = document.createElement('span');
-    let child = document.createElement('span');
+function revealToSspan() {
+    document.querySelectorAll('.reveal').forEach(function(elem){
+        let parent = document.createElement('span');
+        let child = document.createElement('span');
+    
+        parent.classList.add('parent');
+        child.classList.add('child');
+    
+        child.innerHTML = elem.innerHTML;
+        parent.appendChild(child);
+        console.log(parent);
+    
+        elem.innerHTML = "";
+        elem.appendChild(parent);
+    
+    })
+}
 
-    parent.classList.add('parent');
-    child.classList.add('child');
+revealToSspan();
 
-    child.textContent = elem.textContent;
-    parent.appendChild(child);
-    console.log(parent);
+var tl = gsap.timeline()
+.from('.child span', {
+    x: "100",
+    stagger: .2,
+    duration: 1.4,
+    ease: Power3.easeInOut
+})
 
-    elem.innerHTML = "";
-    elem.appendChild(parent);
+.to('.parent .child', {
+    y: "-100%",
+    duration: 1,
+    // delay: 1,
+    ease: Circ.easeInOut,
+})
 
+.to('#loader', {
+    height: 0,
+    duration: 1,
+    // delay: 1,
+    ease: Circ.easeInOut,
+})
+.to('#green', {
+    height: "100%",
+    top: 0,
+    duration: 1,
+    // delay: 1,
+    ease: Circ.easeInOut,
+})
+.to('#green', {
+    height: "0%",
+    top: 0,
+    duration: 1,
+    // delay: 1,
+    ease: Circ.easeInOut,
 })
