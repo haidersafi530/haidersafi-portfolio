1d8be4c93b17cfa6354a59c8bed5f01fbb3c85a3
haidersafi530 <haidersafi2000@gmail.com>
2025-07-26 13:57:58 +0500

    Portfolio Home Page imagery

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+function valueSetteras() {
+    gsap.set('#nav a', {y: "-100%", opacity: 0})
+    gsap.set("#home span .child", {y: '100%'})  
+    gsap.set("#home .row img", {opacity: 0})
+
+    document.querySelectorAll('#Visual > g').forEach(function (group) {
+        // group ke andar <g> hoga, uske andar <path> ya <polyline>
+        const innerGroup = group.querySelector('g');
+        const character = innerGroup?.querySelector('path, polyline');
+      
+        if (character && typeof character.getTotalLength === 'function') {
+          const length = character.getTotalLength();
+          character.style.strokeDasharray = length + "px";
+          character.style.strokeDashoffset = length + "px";
+        }
+      });
+
+}
+
 function revealToSspan() {
     document.querySelectorAll('.reveal').forEach(function(elem){
         let parent = document.createElement('span');
@@ -17,51 +36,91 @@ function revealToSspan() {
 }
 
 function loaderAnimation() {
-    var tl = gsap.timeline()
-.from('.child span', {
+var tl = gsap.timeline()
+tl
+.from('#loader .child span', {
     x: "100",
     stagger: .2,
-    duration: 1.4,
+    duration: 1,
     ease: Power3.easeInOut
 })
 
 .to('.parent .child', {
     y: "-100%",
-    duration: 1,
-    // delay: 1,
+    duration: 0.5,
     ease: Circ.easeInOut,
 })
 
 .to('#loader', {
     height: 0,
-    duration: 1,
-    // delay: 1,
+    duration: 0.5,
     ease: Circ.easeInOut,
 })
 .to('#green', {
     height: "100%",
     top: 0,
-    duration: 1,
-    delay: -.5, 
+    duration: 0.6,
+    delay: -.2, 
     ease: Circ.easeInOut,
 })
 .to('#green', {
     height: "0%",
     top: 0,
-    duration: 1,
-    delay: 1,
+    duration: 0.7,
+    delay: -.2,
     ease: Circ.easeInOut,
+    onComplete: function () {
+        animateHomePage()
+    }
 })
 }
 
-revealToSspan();
-// loaderAnimation();
+function animateSvg() { 
+      gsap.set('#Visual > g > g > path, #Visual > g > g > polyline', function (i, target) {
+        const length = target.getTotalLength();
+        target.style.strokeDasharray = length;
+        target.style.strokeDashoffset = length;
+      });
+      
+      gsap.to('#Visual > g > g > path, #Visual > g > g > polyline', {
+        strokeDashoffset: 0,
+        duration: 3,
+        ease: "expo.inOut",
+        delay: -0.6 ,
+      });
+      
+}
 
-gsap.from('g path', {
-    strokeDasharray: 64.68521881103516,
-    strikeOffset: 64.68521881103516,
-    duration: 1,
-    ease: Power3
-})
+function animateHomePage () {
+    var tl = gsap.timeline();
+
+    tl.to("#nav a", {
+        y: 0,
+        opacity: 1,
+        duration: 0.8,
+        stagger: .05,
+        ease: Expo.easeInOut
+    })
+    tl.to("#home .parent .child", {
+        y: 0,
+        stagger: .1,
+        duration: 1,
+        ease: Expo.easeInOut
+    })
+    tl.to("#home .row img", {
+        opacity: 1,
+        duration:  0.5,    
+        ease: Expo.easeInOut,
+        onComplete: function(){
+            animateSvg();
+        }
+    })  
+}
+
+
+revealToSspan();
+valueSetteras();
+loaderAnimation();
+
 
 
